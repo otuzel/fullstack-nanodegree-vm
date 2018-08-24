8831714073baa475f9dd5f1e6b648367473c1b21
Orkun Tuzel <orkuntuzel@gmail.com>
2018-08-24 16:35:50 +0200

    Routing

--- vagrant/project.py
+++ vagrant/project.py
@@ -1,10 +1,25 @@
 from flask import Flask
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind = engine)
+session = DBSession()
+
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/hello')
-def HelloWorld():
-    return "Hello World"
+@app.route('/restaurants/<int:restaurant_id>/')
+def restaurantMenu(restaurant_id):
+    restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
+    items = session.query(MenuItem).filter_by(restaurant_id = restaurant_id)
+    output = ''
+    for i in items:
+        output += i.name + '</br>'
+        output += i.price + '</br>'
+        output += i.description + '</br></br>'
+    return output
 
 if __name__ == '__main__':
     app.debug = True
